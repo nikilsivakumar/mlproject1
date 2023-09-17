51de4095d032324038e073c58e8dfb2ec907191d
“Nikil <nikildevikumar@gmail.com>
2023-09-17 22:59:28 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,7 +13,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 #tosave train,test,raw data
@@ -64,4 +65,7 @@ if __name__=="__main__":
     train_data,test_data = obj.inititate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -130,3 +130,4 @@ class DataTransformation:
             
         except Exception as e:
             raise CustomException(e,sys)
+ 
